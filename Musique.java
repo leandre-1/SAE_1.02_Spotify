086bd2b2d80f3648840db8b141553bf0952a15c5
Stoneeuuhh <120657548+StoneManga@users.noreply.github.com>
2025-12-01 12:03:24 +0100

    Add files via upload

---
+++ Musique.java
@@ -0,0 +1,15 @@
+package SAE_spotify;
+
+import java.time.LocalDate;
+
+public class Musique {
+    String titre;
+    String id_titre;
+    int duree_titre;
+    String type_album;
+    String nom_Album;
+    LocalDate date_album;
+    String nom_Artiste;
+    int popularite;
+    String artiste;
+}
