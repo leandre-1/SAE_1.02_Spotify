dc3a4ff66b6794a98783acb1e33c6623a3acdd33
Stoneeuuhh <120657548+StoneManga@users.noreply.github.com>
2025-12-12 11:55:01 +0100

    Refactor Musique class and add comparators

--- Musique.java
+++ Musique.java
@@ -1,32 +1,72 @@
 package SAE_spotify;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 
 public class Musique {
-    String titre_Musique;
-    String id_titre;
-    int duree_titre;
-    String type_album;
-    String nom_Album;
-    LocalDate date_album;
-    String nom_Artiste;
-    int popularite;
-    String artiste;
+    public String nom_Musique;
+    public String id_Musique;
+    public int duree_titre;
+    public String type_album;
+    public String nom_Album;
+    public LocalDate date_album;
+    public String nom_Artiste;
+    public int popularite;
+    public String artiste;
 
-    public Musique(String titre, String id, int duree, String type, String nomAlb, LocalDate date, String nomArt, int pop, String art) {
-        titre_Musique = titre;
-        id_titre = id;
-        duree_titre = duree;
-        type_album = type;
-        nom_Album = nomAlb;
-        date_album = date;
-        nom_Artiste = nomArt;
-        popularite = pop;
-        artiste = art;
+    public Musique (String titre, String id_titre, int duree, String type, String nomAl, LocalDate date, String nomArt, int pop, String art){
+            nom_Musique = titre;
+            id_Musique = id_titre;
+            duree_titre = duree;
+            type_album = type;
+            nom_Album = nomAl;
+            date_album = date;
+            nom_Artiste = nomArt;
+            setPopularite(pop);
+            artiste = art;
     }
 
-    public String toString(){
-        return ("Titre : "+titre_Musique+", id du titre : "+id_titre+", Duree du titre : "+duree_titre+", Genre de l'album : "+type_album+
-                ", Nom de l'abum : "+nom_Album+", Nom de l'artiste : "+nom_Artiste+", Popularité : "+popularite+"\n");
+    static public Comparator<Musique> compareAnnee = new Comparator<Musique>() {
+        public int compare(Musique m1, Musique m2) {
+            return m1.date_album.compareTo(m2.date_album);
+        }
+    };
+
+    static public Comparator<Musique> compareChanson = new Comparator <Musique>() {
+        public int compare(Musique m1, Musique m2) {
+            return m1.nom_Musique.compareToIgnoreCase(m2.nom_Musique);
+        }
+    };
+
+    static public Comparator<Musique> compareAlbum = new Comparator <Musique>() {
+        public int compare(Musique m1, Musique m2) {
+            return m1.nom_Album.compareToIgnoreCase(m2.nom_Album);
+        }
+    };
+
+    static public Comparator<Musique> compareArtiste = new Comparator <Musique>() {
+        public int compare(Musique m1, Musique m2) {
+            return m1.nom_Artiste.compareToIgnoreCase(m2.nom_Artiste);
+        }
+    };
+
+    public String toString()
+    {
+        return "\n "+nom_Musique + ", " +
+            nom_Artiste + ", " +
+            nom_Album + ", " +
+            date_album + ", " +
+            duree_titre + " sec, " +
+                getPopularite();
+    }
+
+    public int getPopularite() {
+        return popularite;
+    }
+
+    public void setPopularite(int popularite) {
+        this.popularite = popularite;
     }
 }
+
+    
